777fc7768716cd3c0c29acc9bedf3cbada07bf2e
Andres Ardila <andresardila33@gmail.com>
2023-10-07 09:45:07 -0500

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-center">This is the About page</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1 class="text-center">Homepage</h1>
+    <p class="bg-red text-blue">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
+      incidunt, sequi sapiente, praesentium odit fuga eveniet neque alias
+      placeat odio, vel necessitatibus corrupti! Nisi tempore laborum laboriosam
+      illum autem fugiat.
+    </p>
+    <p class="bg-pink-lighten-4 text-red-darken-4">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+      repellendus dignissimos autem voluptas culpa omnis ducimus inventore
+      exercitationem iste, magnam cupiditate aliquid ut porro aperiam.
+      Asperiores accusantium nulla amet similique?
+    </p>
+    <h1 class="text-h1">Massive display</h1>
+    <h4 class="text-h4">Smaller display</h4>
+    <p class="text-body-1">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae id
+      laboriosam culpa quisquam a, nobis assumenda. Possimus eveniet ex
+      laboriosam nisi sequi unde aut fuga, iste, id optio accusantium veniam?
+    </p>
+    <p class="subtitle-1">This is subtitle</p>
+  </div>
+</template>
